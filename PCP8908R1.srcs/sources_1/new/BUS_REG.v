241eeef1f172e3b3be60070dc44607fa74a73dd2
姚琳 <yaolin0616@163.com>
2024-10-21 13:05:13 +0800

    添加测试寄存器地址为0x100。

--- PCP8908R1.srcs/sources_1/new/BUS_REG.v
+++ PCP8908R1.srcs/sources_1/new/BUS_REG.v
@@ -20,16 +20,53 @@ module BUS (
   wire [15:0] D = io_data_i;
   reg   [31:0] DATA_RD;
 
+  //test regs
+  wire [(16*16 -1):0] test_reg;
+
 // reg read 
 assign io_data_o = DATA_RD;
   always @(*) begin
     case ({io_addr[15:1],1'd0})
     'h0002 : DATA_RD = module_control;   
     'h0004 : DATA_RD = module_status;  
+
+    'h0100 : DATA_RD = test_reg[16*0 +:16];
+    'h0104 : DATA_RD = test_reg[16*1 +:16];
+    'h0108 : DATA_RD = test_reg[16*2 +:16];
+    'h010c : DATA_RD = test_reg[16*3 +:16];
+    'h0110 : DATA_RD = test_reg[16*4 +:16];
+    'h0114 : DATA_RD = test_reg[16*5 +:16];
+    'h0118 : DATA_RD = test_reg[16*6 +:16];
+    'h011c : DATA_RD = test_reg[16*7 +:16];
+    'h0120 : DATA_RD = test_reg[16*8 +:16];
+    'h0124 : DATA_RD = test_reg[16*9 +:16];
+    'h0128 : DATA_RD = test_reg[16*10+:16];
+    'h012c : DATA_RD = test_reg[16*11+:16];
+    'h0130 : DATA_RD = test_reg[16*12+:16];
+    'h0134 : DATA_RD = test_reg[16*13+:16];
+    'h0138 : DATA_RD = test_reg[16*14+:16];
+    'h013c : DATA_RD = test_reg[16*15+:16];
+
     default : DATA_RD = 32'h0;
     endcase
   end
 //reg write
-BUS_CATCH #('h0002,'d2) X0000 (C,BE,A,D, module_control);
+BUS_CATCH #('h0002,'d2) X0002 (C,BE,A,D, module_control);
+BUS_CATCH #('h0100,'d2) X0100 (C,BE,A,D, test_reg[16*0 +:16]);
+BUS_CATCH #('h0104,'d2) X0104 (C,BE,A,D, test_reg[16*1 +:16]);
+BUS_CATCH #('h0108,'d2) X0108 (C,BE,A,D, test_reg[16*2 +:16]);
+BUS_CATCH #('h010c,'d2) X010c (C,BE,A,D, test_reg[16*3 +:16]);
+BUS_CATCH #('h0110,'d2) X0110 (C,BE,A,D, test_reg[16*4 +:16]);
+BUS_CATCH #('h0114,'d2) X0114 (C,BE,A,D, test_reg[16*5 +:16]);
+BUS_CATCH #('h0118,'d2) X0118 (C,BE,A,D, test_reg[16*6 +:16]);
+BUS_CATCH #('h011c,'d2) X011c (C,BE,A,D, test_reg[16*7 +:16]);
+BUS_CATCH #('h0120,'d2) X0120 (C,BE,A,D, test_reg[16*8 +:16]);
+BUS_CATCH #('h0124,'d2) X0124 (C,BE,A,D, test_reg[16*9 +:16]);
+BUS_CATCH #('h0128,'d2) X0128 (C,BE,A,D, test_reg[16*10+:16]);
+BUS_CATCH #('h012c,'d2) X012c (C,BE,A,D, test_reg[16*11+:16]);
+BUS_CATCH #('h0130,'d2) X0130 (C,BE,A,D, test_reg[16*12+:16]);
+BUS_CATCH #('h0134,'d2) X0134 (C,BE,A,D, test_reg[16*13+:16]);
+BUS_CATCH #('h0138,'d2) X0138 (C,BE,A,D, test_reg[16*14+:16]);
+BUS_CATCH #('h013c,'d2) X013c (C,BE,A,D, test_reg[16*15+:16]);
 
 endmodule
